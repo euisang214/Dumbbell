ca33086fde35dbe6650bb9e44e85af0a40a0473a
euisangs2020 <638hb77p>
2018-12-26 17:50:25 +0800

    Initial Commit

---
+++ Microbit_test_0.2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Microbit_test_0.2
+//
+//  Created by Ethan Shin on 26/12/2018.
+//  Copyright © 2018 Ethan Shin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
